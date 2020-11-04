565b7fe00424d25d6220790f3c2d7715379b6719
Christopher Silich <chris.silich@gmail.com>
2020-11-04 12:05:02 -0500

    made it roll something other than D6s

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-   
-	console.log('a user connected');
-	socket.on('disconnect', () => {
-	  console.log('user disconnected');
+io.on("connection", (socket) => {
+	console.log("a user connected");
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
 	});
-	socket.on('request roll', (options) => {
-		console.log('rolling', options);
-		let rolls = []
+	socket.on("request roll", (options) => {
+		console.log("rolling", options);
+		let rolls = [];
 		for (let i = 0; i < options.count; i++) {
-			rolls.push( Math.ceil(Math.random() * 6) )						
+			rolls.push(Math.ceil(Math.random() * options.faces));
 		}
-		io.emit('rolled', {...options, rolls});
+		io.emit("rolled", { ...options, rolls });
 	});
 });
-
-
 
 http.listen(20202, () => {
-	console.log('listening on *:20202');
+	console.log("listening on *:20202");
 });
